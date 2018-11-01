11df2de182a7e6c1a803e9c068b57a77a5c0e68f
Alexandre Falardeau <alexandre.h.falardeau@gmail.com>
2018-10-31 22:39:24 -0400

    Un peu de documentation complété, l'ajout de la nouvelle classe est bien gucci mtn

--- main.js
+++ main.js
@@ -4,7 +4,11 @@
 
 // ======= Helpers for the generators ============
 // ===============================================
-
+/**
+ * This function creates a classified icon element
+ * It also colors the icon appropriately
+ * @param {String} type: The name of the icon
+ */
 function makeIcon(type) {
     let icon = document.createElement("i");
     icon.classList.add("fas");
@@ -21,12 +25,24 @@ function makeIcon(type) {
 // ============== Generators =====================
 // ===============================================
 
+/**
+ * This funciton creates a div element that has a class
+ * @function
+ * @param {String} className : the name of the class for the div
+ */
 function classfiedDivGenerator(className) {
     let div = document.createElement("div");
     div.classList.add(className);
     return div;
 }
 
+/**
+ * This function creates an element that will act as a group 
+ * option button in the group list function of the html
+ * @function
+ * @param {String} groupName : the name of the group 
+ * @param {String} type : The type of icon that will be put next to the group name
+ */
 function groupOptionGenerator(groupName, type = "star") {
     let element = classfiedDivGenerator("group-selector");
     let icon = makeIcon(type);
@@ -39,11 +55,47 @@ function groupOptionGenerator(groupName, type = "star") {
     return element;
 }
 
+function newGroupFormGenerator() {
+    let newGroupForm = document.createElement("div");
+    newGroupForm.id = "new-group-form";
+    let form = document.createElement("form");
+    let groupSelector = classfiedDivGenerator("group-selector");
+    let button = document.createElement("button");
+    button.appendChild(makeIcon("plus-square"));
+    groupSelector.appendChild(button);
+    let input = document.createElement("input");
+    input.setAttribute("type", "text")
+    input.setAttribute("name", "newGroup");
+    groupSelector.appendChild(input);
+    form.appendChild(groupSelector);
+    form.addEventListener("submit", handleAddNewGroup);
+    newGroupForm.appendChild(form);
+    return newGroupForm;
+}
+
+/**
+ * @todo: Finish this function
+ * This function creates
+ * @function This function creates an element for the chat view
+ * in which all the messages will be displayed
+ * @param {String} groupName : The name of the group
+ */
 function chatViewGenerator(groupName) {
     console.log("chatViewGenerator needs to be built");
     let box = document.createElement("div");
     box.innerText = groupName;
     return box;
+}
+
+/**
+ * @todo: implement this function
+ * @function: This function generate a messagebox element
+ * @param {String} message 
+ * @param {String} user 
+ * @param {Stirng} timeStamp 
+ */
+function messageGenerator(message, user, timeStamp) {
+
 }
 
 // =========================================
@@ -53,6 +105,12 @@ function chatViewGenerator(groupName) {
 // ============ Model ======================
 // =========================================
 
+/**
+ * @todo: Ask if this is still considered a class
+ * @class
+ * 
+ * 
+ */
 var Model = {
     // There will be a groupList view
     // There will be a newGroupForm view
@@ -67,9 +125,17 @@ var Model = {
     activeGroup: null, //The current group
     previousGroup: null, //The previous group
 
+    /**
+     * This function will initialize the model and create trigger the 
+     * creation of all of the necessary views that need to present on page load
+     * @todo: Ask if this counts as a constructor
+     * @method
+     * @constructor
+     * @param {Array[Strings]} groups : The groups that are initialized on page load 
+     */
     __init__: function(groups) {
         this.views.groupList = groupListView;
-        this.views.newGroupForm = newGroupFormView; 
+        //this.views.newGroupForm = newGroupFormView; 
         groups.forEach((groupName)=>{
             this.addGroup(groupName);
         });
@@ -79,6 +145,13 @@ var Model = {
 
         // Should I update the view from here????
     },
+
+    /**
+     * This method adds a group to the Model by creating the chat view
+     * and placing it in its views object property
+     * @method
+     * @param {String} groupName 
+     */
     addGroup: function(groupName){
         this.groupViews.push(groupName);
         let newGroupChatView = Object.create(groupChatView);
@@ -86,29 +159,34 @@ var Model = {
         this.views[groupName] = newGroupChatView;
     }
 };
-
+/** @event: This event is triggers when a user changes group */
 var changeGroup = new Event("changeGroup");
-var newGroup = new Event("newGroup");
 
+/** @listens: for the changeGroup event and toggles the displays
+ * of the olf view and the new view (now the current view);
+ */
 document.addEventListener("changeGroup", function() {
     Model.previousGroup.toggleDisplay();
     Model.activeGroup.toggleDisplay();
     Model.activeGroup.changeHeader();
 })
 
+/** @event: This event is triggers when a user creates a group*/
+var newGroup = new Event("newGroup");
+/** @listens: for the newGroup event and updates the groupList view */
 document.addEventListener("newGroup", function() {
-    Model.views.newGroupForm.toggleDisplay();
+    
+    //Model.views.newGroupForm.toggleDisplay();
+    Model.views.groupList.toggleForm();
     Model.views.groupList.update();
 })
 
 
+//======================================================
+//================= Views ==============================
+//======================================================
 
 
-
-
-
-
-// Views================================
 // Abstract class (ish)
 var View = {
     node: null, //Implemented in child
@@ -121,8 +199,9 @@ var View = {
 }
 
 
-// VIEWS ========================
-// Group List view definition
+/**
+ * @class groupoListView
+ */
 var groupListView = Object.create(View);
 
 groupListView.node = document.querySelector("#group-interface");
@@ -149,17 +228,32 @@ groupListView.update = function() {
         iconNode.classList.remove("fa-minus");
         iconNode.classList.add("fa-plus");
         // Add the new class
-        this.contentNode.insertBefore(groupOption, defaultGroupNode.nextElementSibling);
+        this.addAfterDefaultGroup(groupOption);
     }
     else {
         this.contentNode.appendChild(groupOption);
     }
-
-    
-    
 }
 
-// Group chat view defintion
+groupListView.addAfterDefaultGroup = function(element) {
+    let defaultGroupNode = this.contentNode.firstElementChild;
+    this.contentNode.insertBefore(element, defaultGroupNode.nextElementSibling);
+}
+
+groupListView.toggleForm = function() {
+    let node = document.querySelector("#new-group-form");
+    if (node) { //If the node points to somewhere
+        node.parentNode.removeChild(node) //essentially removes itself
+    }
+    else {
+        let form = newGroupFormGenerator();
+        this.addAfterDefaultGroup(form);
+    }
+}
+
+/**
+ * @class GroupChatView
+ */
 
 var groupChatView = Object.create(View);
 
@@ -175,10 +269,6 @@ groupChatView.__init__ = function(groupName) {
     this.node = this.contentNode.lastElementChild;
 };
 
-// groupChatView.toggleDisplay = function() {
-
-// }
-
 groupChatView.changeHeader = function() {
     let headerTitle = this.headerNode.lastElementChild;
     headerTitle.innerText = this.groupName;
@@ -188,12 +278,18 @@ groupChatView.update = function() {
     // This has to do with chat functions
 }
 
-// Group Form View definition
+/**
+ * @class : newGroupFormView
+ */
 
-var newGroupFormView = Object.create(View);
-newGroupFormView.node = document.querySelector("#new-group-form");
-// This gets the form
-newGroupFormView.node.firstElementChild.addEventListener("submit", handleAddNewGroup);
+// var newGroupFormView = Object.create(View);
+// newGroupFormView.node = document.querySelector("#new-group-form");
+// // This gets the form
+// newGroupFormView.node.firstElementChild.addEventListener("submit", handleAddNewGroup);
+
+// newGroupFormView.toggleDisplay() = function() {
+
+// }
 
 // Has to have an update function that can append a message child (of the correct type
 // and format) tot the view, while updating the position of the rest
@@ -215,7 +311,7 @@ let plusIcon = document.querySelector("#group-interface .header .fas.fa-plus");
 plusIcon.addEventListener("click", handleCreateNewGroup);
 
 function handleCreateNewGroup(event) {
-    Model.views.newGroupForm.toggleDisplay();
+    Model.views.groupList.toggleForm();
 }
 
 // This is a controller function
